369545629b5466e4fa4b7b54481152188c96793f
Markku Kinnunen <kinexdev@gmail.com>
2020-02-12 23:46:58 +0200

    - updated license info

--- android/src/main/kotlin/com/kineapps/flutterarchive/FlutterArchivePlugin.kt
+++ android/src/main/kotlin/com/kineapps/flutterarchive/FlutterArchivePlugin.kt
@@ -1,3 +1,8 @@
+// Copyright (c) 2020 KineApps. All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
 package com.kineapps.flutterarchive
 
 import android.util.Log
